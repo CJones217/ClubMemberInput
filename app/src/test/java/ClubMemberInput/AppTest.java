8237d296e4680ffdab1dfefec3dbc8caf1fe15e7
kphillips011 <kendallnicole011@gmail.com>
2021-05-01 13:09:00 -0500

    added to AppTest.java, test should pass

--- app/src/test/java/ClubMemberInput/AppTest.java
+++ app/src/test/java/ClubMemberInput/AppTest.java
@@ -14,6 +14,6 @@ public class AppTest {
 
     @Test public void testAppGreetingHelloWorld() {
         App classUnderTest = new App();
-        assertEquals("Hello World",  classUnderTest.getGreeting());
+        assertEquals("Hello World!",  classUnderTest.getGreeting());
     }
 }
